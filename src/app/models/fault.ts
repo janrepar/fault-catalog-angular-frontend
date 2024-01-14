ea5866816ef6eeca3be2ec57d8f63c091523649d
Jan Repar <jan.repar@outlook.com>
2024-01-14 13:32:59 +0100

    Added models fault.ts and success-criterion.ts.

---
+++ src/app/models/fault.ts
@@ -0,0 +1,9 @@
+export class Fault {
+    id?: number;
+    title: string = "";
+    shortDescription: string = "";
+    description: string = "";
+    successCriterionRefIds: string[] = [];
+    
+    // ?TODO: Dodaj se placehoderje za posnetek zaslona, url, element, html koda (jih vpiše uprabnik - pomembno za zapisnik)
+}
---
+++ src/app/models/success-criterion.ts
@@ -0,0 +1,7 @@
+export class SuccessCriterion {
+    refId: string = "";
+    title: string = "";
+    description: string = "";
+    url: string = "";
+    level: string = "";
+}
